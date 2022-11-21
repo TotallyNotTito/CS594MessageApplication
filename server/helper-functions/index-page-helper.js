f69981d15e071b08c836e630e1a0071c0cd3b958
Joshua Wachana <jjwachana@gmail.com>
2022-11-20 18:17:57 -0800

    creating a room on index page and adding it to room relections using form submission, just text based, not functional whatsoever yet.

---
+++ server/helper-functions/index-page-helper.js
@@ -0,0 +1,2 @@
+const indexPageRooms = ['Test Room 1', 'Test Room 2'];
+module.exports = indexPageRooms;
--- server/server.js
+++ server/server.js
@@ -2,9 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+const indexPageRooms = require('./helper-functions/index-page-helper');
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { indexPageRooms });
+});
+
+app.post('/add-room-to-index', (req, res) => {
+  room = req.body.indexCreateRoom;
+  indexPageRooms.unshift(room);
+  console.log(indexPageRooms);
+  res.redirect('/');
 });
 
 app.get('/converse', (req, res) => {
