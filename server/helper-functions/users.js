5be8b2c5fc8190eeca913d2b0cddf882990c4636
Joshua Wachana <jjwachana@gmail.com>
2022-11-28 23:41:55 -0800

    client can join multiple rooms and send distinct messages to different rooms

--- server/helper-functions/users.js
+++ server/helper-functions/users.js
@@ -27,4 +27,44 @@ const getUsersInRoom = (room) => {
   return usersArray.filter((user) => user.room === room);
 };
 
-module.exports = { joinUser, getCurrUser, leaveUser, getUsersInRoom };
+//additional room
+
+const usersArray2 = [];
+
+const joinUser2 = (id, username, room) => {
+  const userObject2 = {
+    id: id,
+    username: username,
+    room: room,
+  };
+  usersArray2.push(userObject2);
+  return userObject2;
+};
+
+const getCurrUser2 = (id) => {
+  const currentUser2 = usersArray2.find((user) => user.id === id);
+  return currentUser2;
+};
+
+const leaveUser2 = (id) => {
+  const index2 = usersArray2.findIndex((user) => user.id === id);
+
+  if (index2 !== -1) {
+    return usersArray2.splice(index2, 1)[0];
+  }
+};
+
+const getUsersInRoom2 = (room) => {
+  return usersArray2.filter((user) => user.room === room);
+};
+
+module.exports = {
+  joinUser,
+  getCurrUser,
+  leaveUser,
+  getUsersInRoom,
+  joinUser2,
+  getCurrUser2,
+  leaveUser2,
+  getUsersInRoom2,
+};
